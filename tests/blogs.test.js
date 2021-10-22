209482ac37af78fc3fde50a7b6e2d729a5d72d70
JuraCik <ykac@cilum.com>
2021-10-22 17:30:36 +0300

    Tests for logged in cases;

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -18,6 +18,43 @@ describe('When logged in', async () => {
         const titleLabel = await page.getContentsOf('form label');
         expect(titleLabel).toEqual('Blog Title');
     });
+
+    describe('and using invalid inputs', async () => {
+        beforeEach(async () => {
+            await page.click('form button');
+        });
+        test('the form shows error', async () => {
+            const titleError = await page.getContentsOf('.title .red-text');
+            const contentError = await page.getContentsOf('.content .red-text');
+
+            expect(titleError).toEqual('You must provide a value')
+            expect(contentError).toEqual('You must provide a value')
+        });
+    });
+
+    describe('adn using valid inputs', async () => {
+        beforeEach(async () => {
+            await page.type('.title input', 'My title');
+            await page.type('.content input', 'My content');
+            await page.click('form button');
+        });
+
+        test('Submitting takes user to review page', async () => {
+            const confirmText = await page.getContentsOf('form h5');
+
+            expect(confirmText).toEqual('Please confirm your entries')
+        });
+        test('Submitting then saving takes user to blogs page', async () => {
+            await page.click('button.green');
+            await page.waitFor('.card');
+
+            const title = await page.getContentsOf('.card-title');
+            const content = await page.getContentsOf('p');
+
+            expect(title).toEqual('My title');
+            expect(content).toEqual('My content');
+        });
+    });
 });
 
 afterEach(async () => {
