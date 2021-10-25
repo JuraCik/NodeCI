6629c1eebd4e65c198f3582be84d748c686bb0ea
JuraCik <ykac@cilum.com>
2021-10-25 15:17:32 +0300

    travis CI set up

--- config/dev.js
+++ config/dev.js
@@ -4,4 +4,5 @@ module.exports = {
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI: "mongodb://jura:password24@cluster0-shard-00-00.sb9ti.mongodb.net:27017,cluster0-shard-00-01.sb9ti.mongodb.net:27017,cluster0-shard-00-02.sb9ti.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-xzo9t5-shard-0&authSource=admin&retryWrites=true&w=majority",
   cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
 };
